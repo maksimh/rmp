b4f7f9fc801db9d13300e953acceb18633e717d0
Maksim H <maksimh@yahoo.com>
2017-06-04 20:59:23 +0300

    Validate that account has valid account type

--- app/models/rmp/account.rb
+++ app/models/rmp/account.rb
@@ -1,4 +1,7 @@
 module Rmp
   class Account < ApplicationRecord
+    validates_inclusion_of :account_type,
+      in: ['asset', 'equity', 'liability', 'revenue', 'expense'],
+      message: 'Account type must be asset, equity, liability, revenue or expense'
   end
 end

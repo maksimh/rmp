494f3628d2055e1fc2e2d0ca4a7f3ead9ea68f6f
Maksim H <maksimh@yahoo.com>
2017-06-04 20:51:00 +0300

    Test that account has valid account type

--- test/models/rmp/account_test.rb
+++ test/models/rmp/account_test.rb
@@ -2,8 +2,24 @@ require 'test_helper'
 
 module Rmp
   class AccountTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+    test "invalid with no account types" do
+      account = Account.new
+      assert_not account.valid?
+      assert_equal ["Account type must be asset, equity, liability, revenue or expense"], account.errors.messages[:account_type]
+    end
+
+    test "valid with one of five allowed account types" do
+      account = Account.new
+      %w(asset equity liability revenue expense).each do |valid_type|
+        account.account_type = valid_type
+        assert account.valid?
+      end
+    end
+
+    test "invalid with not allowed account types" do
+      account = Account.new
+      account.account_type = 'string'
+      assert_not account.valid?
+    end
   end
 end
